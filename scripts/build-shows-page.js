c42dc6f2dd97f9eac38a44c735095b83c21a6b87
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-01-28 22:40:54 -0500

    commit two - addition of shows section

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,82 @@
+const shows = [
+    {
+      date: "Mon Sept 09 2024",
+      venue: "Ronald Lane",
+      location: "San Francisco, CA",
+    },
+    {
+      date: "Tues Sept 17 2024",
+      venue: "Pier 3 East",
+      location: "San Francisco, CA",
+    },
+    {
+      date: "Sat Oct 12 2024",
+      venue: "View Lounge",
+      location: "San Francisco, CA",
+    },
+    {
+      date: "Sat Nov 16 2024",
+      venue: "Hyatt Agency",
+      location: "San Francisco, CA",
+    },
+    {
+      date: "Fri Nov 29 2024",
+      venue: "Moscow Center",
+      location: "San Francisco, CA",
+    },
+    {
+      date: "Wed Dec 18 2024",
+      venue: "Press Club",
+      location: "San Francisco, CA",
+    },
+  ];
+
+//   Notes
+// const list = document.getElementById('show-list')
+
+// for (const show of shows) {
+//     const show = document.createElement("div");
+//     show.textContent = showObj.venue + "-" + showObj.date;
+//     // OR
+//     show.textContent = showObj.date;
+    
+//     list.append(show);
+// }
+
+// //   Notes
+// const list = document.getElementById('show-list')
+
+// for (let i = 0; i < shows.length; i++) {
+//     const showObj = shows[i]
+// const show = document.createElement ("div");
+// show.classList.add("show");
+// show.textContent = showObj.venue + "-" + showObj.date;
+// list.appendChild(show);
+// }
+
+// Notes
+// const list = document.getElementById('show-list')
+
+// for (let i = 0; i < shows.length; i++) {
+//   const showObj = shows[i]
+
+//   const show = document.createElement ("div");
+//   show.classList.add("show");
+
+//   show.textContent = showObj.venue + showObj.date;
+// }
+
+
+// Notes
+const list = document.getElementById('show-list')
+console.log(list)
+
+for (const showObj of shows) {
+  const show = document.createElement ("div");
+  show.classList.add("show");
+  show.textContent = showObj.venue + "-" + showObj.date;
+
+  list.append(show);
+}
+
+// Deffo need to use querySelector somewhere
